4010dbe04b466750638e0ab993b0a900de0b366e
Alexander <renyreny125tt@gmail.com>
2022-07-10 21:37:46 +0300

    feat: add firestore functions

--- functions/index.js
+++ functions/index.js
@@ -1,37 +1,31 @@
 const functions = require("firebase-functions");
-// const admin = require('firebase-admin');
+const admin = require('firebase-admin');
 // const { getDatabase } = require('firebase-admin/database');
 
 // Get a database reference to our blog
-// admin.initializeApp();
-const { initializeApp, applicationDefault } = ('firebase-admin/app');
+admin.initializeApp();
+// const { initializeApp, applicationDefault } = ('firebase-admin/app');
 
-initializeApp({
-  credential: applicationDefault(),
-  projectId: '<nodefirebase-b1a6b>',
-});
-// const db = getDatabase();
+// initializeApp({
+//   credential: applicationDefault(),
+//   projectId: '<nodefirebase-b1a6b>',
+// });
+// const db = admin.firestore();
 // const ref = db.ref('nodefirebase-b1a6b/us-central1/updateUser');
 
 
 
-// exports.requestUser = functions.https.onRequest(async (req, res) => {
-//   const id = req.query.id;
+exports.requestUser = functions.https.onRequest(async (req, res) => {
+  const id = req.query.id;
   
-//   const queryUser = await admin.firestore().collection('users').where('id', '==', id).get();
-//   res.json(queryUser);
-// });
-
-exports.updateUser = functions.https.onRequest(async (req, res) => {
-
-  // const usersRef = ref.child('users');
-  // usersRef.set({
-  //   alanisawesome: {
-  //     date_of_birth: 'June 23, 1912',
-  //     full_name: 'Alan Turing'
-  //   }
-  // });
-  // const user = req.body;
-  // const use = await admin.firestore().collection('users').add(user);
-  res.json('ok');
+  const queryUser = await admin.firestore().collection('users').where('id', '==', id).get();
+  res.json(queryUser);
 });
+
+exports.createUser = functions.https.onRequest(async (req, res) => {
+   const data = req.body;
+
+   const saveUser = await admin.firestore().collection('users').add(data);
+   res.json(saveUser);
+});
+    
